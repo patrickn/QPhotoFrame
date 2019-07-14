1a048406260ebc5570cd486cb70a841a2bd861c7
Patrick <patrick@neavey.net>
2019-07-14 19:50:38 +0200

    Rename WeatherData to WeatherModel.

--- WeatherModel/WeatherModel.cpp
+++ WeatherModel/WeatherModel.cpp
@@ -1,15 +1,16 @@
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
+#include <QJsonArray>
 #include <QJsonDocument>
 #include <QJsonObject>
 #include <QJsonValue>
 #include <QNetworkConfigurationManager>
 #include <QNetworkReply>
 #include <QUrlQuery>
-#include "WeatherData.h"
+#include "WeatherModel.h"
 //-----------------------------------------------------------------------------
 
-WeatherData::WeatherData(QObject* parent)
+WeatherModel::WeatherModel(QObject* parent)
     : QObject(parent),
       _src(nullptr),
       _nam(nullptr),
@@ -27,7 +28,7 @@ WeatherData::WeatherData(QObject* parent)
     _ns->open();
 }
 
-WeatherData::~WeatherData()
+WeatherModel::~WeatherModel()
 {
     _ns->close();
     if (_src) {
@@ -35,45 +36,62 @@ WeatherData::~WeatherData()
     }
 }
 
-bool WeatherData::ready() const
+bool WeatherModel::ready() const
 {
     return _ready;
 }
 
-bool WeatherData::hasSource() const
+bool WeatherModel::hasSource() const
 {
     return (_src != nullptr);
 }
 
-void WeatherData::hadError(bool tryAgain)
+void WeatherModel::hadError(bool tryAgain)
 {
     qDebug() << "hadError, will " << (tryAgain ? "" : "not ") << "rety";
 }
 
-void WeatherData::refreshWeather()
+void WeatherModel::refreshWeather()
 {
     qDebug() << "WeatherData::refreshWeather()";
-    qDebug() << "City: " << _city;
+
+    if (_city.isEmpty()) {
+        qDebug() << "Cannot refresh weather, no city";
+        return;
+    }
+
+    QUrl url("http://api.openweathermap.org/data/2.5/weather");
+    QUrlQuery query;
+
+    query.addQueryItem("q", _city);
+    query.addQueryItem("mode", "json");
+    query.addQueryItem("APPID", QStringLiteral("36496bad1955bf3365448965a42b9eac"));
+    url.setQuery(query);
+
+    QNetworkReply* rep = _nam->get(QNetworkRequest(url));
+    connect(rep, &QNetworkReply::finished, this, [this, rep]() {
+        handleWeatherNetworkData(rep);
+    });
 }
 
-bool WeatherData::hasValidCity() const
+bool WeatherModel::hasValidCity() const
 {
     return (!(_city.isEmpty()) && _city.size() > 1 && _city != "");
 }
 
-QString WeatherData::city() const
+QString WeatherModel::city() const
 {
     return _city;
 }
 
-void WeatherData::setCity(const QString& value)
+void WeatherModel::setCity(const QString& value)
 {
     _city = value;
     emit cityChanged();
     refreshWeather();
 }
 
-void WeatherData::queryCity()
+void WeatherModel::queryCity()
 {
     qDebug() << "WeatherData::queryCity()";
 
@@ -99,7 +117,7 @@ void WeatherData::queryCity()
     });
 }
 
-void WeatherData::networkSessionOpened()
+void WeatherModel::networkSessionOpened()
 {
     _src = QGeoPositionInfoSource::createDefaultSource(this);
 
@@ -112,7 +130,7 @@ void WeatherData::networkSessionOpened()
     }
 }
 
-void WeatherData::positionUpdated(QGeoPositionInfo gpsPos)
+void WeatherModel::positionUpdated(QGeoPositionInfo gpsPos)
 {
     qDebug() << "WeatherData::positionUpdated(...)";
 
@@ -120,12 +138,12 @@ void WeatherData::positionUpdated(QGeoPositionInfo gpsPos)
     queryCity();
 }
 
-void WeatherData::positionError(QGeoPositionInfoSource::Error e)
+void WeatherModel::positionError(QGeoPositionInfoSource::Error e)
 {
     qDebug() << "WeatherData::positionError(...)";
 }
 
-void WeatherData::handleGeoNetworkData(QNetworkReply* networkReply)
+void WeatherModel::handleGeoNetworkData(QNetworkReply* networkReply)
 {
     qDebug() << "WeatherData::handleGeoNetworkData(...)";
 
@@ -154,4 +172,66 @@ void WeatherData::handleGeoNetworkData(QNetworkReply* networkReply)
         hadError(true);
     }
     networkReply->deleteLater();
+}
+
+void WeatherModel::handleWeatherNetworkData(QNetworkReply* networkReply)
+{
+    qDebug() << "WeatherData::handleWeatherNetworkData(...)";
+
+    if (!networkReply) {
+        return;
+    }
+
+    if (!networkReply->error()) {
+//        foreach (WeatherData* inf, _forecast) {
+//            delete inf;
+//        }
+//        _forecast.clear();
+
+        QJsonDocument document = QJsonDocument::fromJson(networkReply->readAll());
+
+        if (document.isObject()) {
+            QJsonObject obj = document.object();
+            QJsonObject tempObject;
+            QJsonValue val;
+
+            if (obj.contains(QStringLiteral("weather"))) {
+                val = obj.value(QStringLiteral("weather"));
+                QJsonArray weatherArray = val.toArray();
+                val = weatherArray.at(0);
+                tempObject = val.toObject();
+//                _now.setWeatherDescription(tempObject.value(QStringLiteral("description")).toString());
+//                _now.setWeatherIcon(tempObject.value("icon").toString());
+            }
+            if (obj.contains(QStringLiteral("main"))) {
+                val = obj.value(QStringLiteral("main"));
+                tempObject = val.toObject();
+                val = tempObject.value(QStringLiteral("temp"));
+//                _now.setTemperature(niceTemperatureString(val.toDouble()));
+            }
+        }
+    }
+    networkReply->deleteLater();
+
+    //retrieve the forecast
+//    QUrl url("http://api.openweathermap.org/data/2.5/forecast/daily");
+//    QUrlQuery query;
+
+//    query.addQueryItem("q", d->city);
+//    query.addQueryItem("mode", "json");
+//    query.addQueryItem("cnt", "5");
+//    query.addQueryItem("APPID", d->app_ident);
+//    url.setQuery(query);
+
+//    QNetworkReply *rep = d->nam->get(QNetworkRequest(url));
+//    // connect up the signal right away
+//    connect(rep, &QNetworkReply::finished, this, [this, rep]() {
+//        handleForecastNetworkData(rep);
+//    });
+}
+
+void WeatherModel::handleForecastNetworkData(QNetworkReply* networkReply)
+{
+    qDebug() << "WeatherData::handleForecastNetworkData(...)";
+
 }
