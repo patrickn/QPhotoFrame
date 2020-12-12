f0b3cc968215fb0339fe3898611ff8f5a1b6cc32
Patrick <patrick@neavey.net>
2020-12-12 18:59:23 +0100

    Add version files.

---
+++ Common/Version.cpp
@@ -0,0 +1,14 @@
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+#include <Version.h>
+//-----------------------------------------------------------------------------
+
+namespace qphotoframe 
+{
+   const int MAJOR_VERSION = 0;
+   const int MINOR_VERSION = 1;
+   const int PATCH_VERSION = 0;
+   const int COMMITS_AHEAD_OF_VERSION = 0;
+   const char* VERSION_STRING = "0.1.0";
+   const char* VERSION_STRING_PLUS_COMMITS = "0.1.0+0";
+}
